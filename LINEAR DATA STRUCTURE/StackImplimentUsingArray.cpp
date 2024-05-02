5a175cf11ad2a0850b76de84625f691200fe7aac
Neeraj Kumar Thakur <neerajkumarthakur.dhaka@gmail.com>
2024-05-02 23:40:01 +0530

    first commit

--- LINEAR DATA STRUCTURE/StackImplimentUsingArray.cpp
+++ LINEAR DATA STRUCTURE/StackImplimentUsingArray.cpp
@@ -1,89 +1,75 @@
-// Stack implimentation using array in c++
+// Stack implimentation using array
 
 #include<iostream>
 using namespace std;
-    int stack[5],n=5,top=-1;
+    int stack[5],size=5,top=-1;
 
     void push()
     {
         int x;
-       if(top==n-1)
-        cout<<"\nStack is ful"<<endl;
-
-       else{
-        cout<<"Enter the element in stack :";
-        cin>>x;
-        top++;
-        stack[top]=x;
-       }
+        if(top==size-1)
+        {
+            cout<<"Stack is Overflow"<<endl;
+        }
+        else{
+            cout<<"Enter an element in stack:";
+            cin>>x;
+            top++;
+            stack[top]=x;
+        }
     }
 
     void pop()
     {
-       // int n;
-        if(top==-1)
+        if (top==-1)
         {
-            cout<<"Stack is Empty";
+            cout<<"Stack is Empty"<<endl;
         }
         else{
             stack[top];
-
-            cout<<"The popped element is :"<<stack[top]<<endl;
+            cout<<"The delete element is :"<<stack[top];
             top--;
         }
     }
 
     void display()
     {
-        
-        if(top==-1){
-           cout<<"Stack is empty ";
-
+        if(top==-1)
+        {
+            cout<<"Stack is Empty "<<endl;
         }
-         else
-         {
-            cout<<"Element of stack :";
+        else{
+            cout<<"The all element in stack :"<<endl;
             for(int i=top;i>=0;i--)
             cout<<stack[i]<<endl;
-         }
-      
+        }
     }
 
     int peek()
     {
         return stack[top];
     }
-
-    int main()
-    {
+    
+       int main()
+       {
         int choise;
-        do
-        {
-        cout<<"\n__________________Circular Queue Menu________________";
-        cout<<"\n1.push\n2.pop\n3.display\n4.peek";
-       cout<<"\n___________________________________________________"<<endl;
+        do{
+        cout<<"\n_____________________neme________________";
+        cout<<"\n1.push  2.pop  3.display  4.peek"<<endl;
         cout<<"\nEnter your choise :";
         cin>>choise;
         switch(choise)
         {
-            case 1:push();
-            break;
-            case 2:pop();
-            break;
-            case 3:display();
-            break;
-            case 4:int y=peek();
-            cout<<"The top element of the stack is : "<<y<<endl;
+            case 1:push();break;
+            case 2:pop();break;
+            case 3:display();break;
+            case 4:peek();
+            cout<<"The top element is :"<<stack[top]<<endl;
             break;
         }
-    }
+        }
         while(choise!=5);
 
-
         return 0;
-    }
+       }
 
-    
-
-
-  
